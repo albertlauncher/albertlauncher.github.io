d65dcb22111569df448a27354e80e9124f0572c4
Manuel Schneider <manuelschneid3r@gmail.com>
2017-01-29 15:34:59 +0100

    Update website

--- js/themecreator/generator.js
+++ js/themecreator/generator.js
@@ -1,47 +1,108 @@
-var clipboard = new Clipboard(".theme button", {
-    text: function () {
-        return document.getElementById("result").innerText;
+var themes, defaultTheme = "Bright";
+
+main();
+
+/**
+ * This is the begining.
+ */
+function main() {
+    initClipboard();
+    loadThemes(function () {
+        // And finally display albert to the public
+        document.getElementById('albert').style.display = 'block';
+        document.getElementById('options').style.display = 'block';
+    });
+
+    // Get all inputs with the 'style' class and add an event listener
+    var inputs = [].slice.call(document.querySelectorAll('.style'));
+    inputs.forEach(function (input) {
+        input.addEventListener('input', function () {
+            updateTheme(input.name, input.value);
+        });
+    });
+}
+
+/**
+ * Init the clipboard plugin.
+ */
+function initClipboard() {
+    var clipboard = new Clipboard(".theme button", {
+        text: function () {
+            return document.getElementById("result").innerText;
+        }
+    });
+
+    clipboard.on('success', function (e) {
+        var trigger = e.trigger,
+            original = trigger.innerHTML;
+
+        trigger.innerHTML = "Copied!";
+        setTimeout(function () {
+            trigger.innerHTML = original;
+        }, 1000);
+    });
+}
+
+function loadThemes(callback) {
+    var XmlHttpRequest = new XMLHttpRequest();
+    XmlHttpRequest.overrideMimeType("application/json");
+    XmlHttpRequest.open('GET', '../themes.json', true);
+    XmlHttpRequest.onreadystatechange = function () {
+        if (XmlHttpRequest.readyState == 4 && XmlHttpRequest.status == "200") {
+            themes = JSON.parse(XmlHttpRequest.responseText);
+
+            // We set the first theme to be the base theme, since it's the default one in albert
+            for (var prop in themes[defaultTheme]) {
+                updateTheme(prop, themes[defaultTheme][prop]);
+
+                var els = document.querySelectorAll("[name='" + prop + "']");
+                for (var i = 0; i < els.length; i++) {
+                    els[i].value = themes[defaultTheme][prop];
+                }
+            }
+
+            callback();
+        }
     }
-});
+    XmlHttpRequest.send(null);
+}
 
-clipboard.on('success', function (e) {
-    var trigger = e.trigger,
-        original = trigger.innerHTML;
+/**
+ * Update the theme with the user's changes.
+ *
+ * @param el
+ */
+function updateTheme(propName, propValue) {
+    var original = propValue;
 
-    trigger.innerHTML = "Copied!";
-    setTimeout(function () {
-        trigger.innerHTML = original;
-    }, 1000);
-});
+    if (propName == 'item_selected_color' || propName == 'action_list_selected_color') {
+        propValue = hexToRgbA(propValue, 0.2);
+    }
 
-var inputs = document.getElementsByTagName('input'),
-    variables = new Object;
+    // Update the style in realtime
+    document.documentElement.style.setProperty('--' + propName, propValue, "");
 
-for (var i = 0; i < inputs.length; i++) {
-    if ((' ' + inputs[i].className + ' ').indexOf(' style ') > -1) {
-        var name = inputs[i].name,
-            value = inputs[i].value;
-
-        variables["@" + name] = value;
-        updateTheme(name, value);
-
-        inputs[i].addEventListener('input', function (e) {
-            var name = e.target.name,
-                value = e.target.value;
-
-            variables["@" + name] = value;
-
-            updateTheme(name, value);
-            less.modifyVars(variables);
-            less.refreshStyles();
-        }, false);
+    // Update the to be qss file
+    var els = document.querySelectorAll('.' + propName);
+    for (var i = 0; i < els.length; i++) {
+        original = original.replace(/#/g, '');
+        els[i].innerHTML = original;
     }
 }
 
-function updateTheme(variable, value) {
-    var els = document.getElementsByClassName(variable);
-    for (var i = 0; i < els.length; i++) {
-        value = value.replace(/#/g, '');
-        els[i].innerHTML = value;
+/**
+ *
+ * @returns {string}
+ */
+function hexToRgbA(hex, transparency) {
+    var c;
+    if (/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)) {
+        c = hex.substring(1).split('');
+        if (c.length == 3) {
+            c = [c[0], c[0], c[1], c[1], c[2], c[2]];
+        }
+        c = '0x' + c.join('');
+        return 'rgba(' + [(c >> 16) & 255, (c >> 8) & 255, c & 255].join(',') + ', ' + transparency + ')';
     }
+    throw new Error('Bad Hex');
 }
